629257b312e9416e41af1037877a54bdf4641149
Jorge Castilla <jorgecastillagallardo@gmail.com>
2023-12-13 20:20:50 -0300

    Actualización clientes, logo, estilo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -9,10 +7,6 @@ export default defineConfig({
   site: "https://egx-engineer.vercel.app",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
     sitemap(),
   ],
 });
